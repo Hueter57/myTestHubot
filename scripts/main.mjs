6dfb48b5348c72f78cf6502cac51fbc249a28036
Hueter <kohsukemori@gmail.com>
2025-02-16 19:31:29 +0900

    🍥 fix stamp maybe

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -12,44 +12,30 @@ export default async robot => {
   robot.respond(/ping$/i, res => {
     res.reply("pong");
   });
+
   robot.hear(/send_Hueter$/, res => {
-    res.send(
-      {
-        type: "stamp",
-        name: "done-nya"
-      }, {
-        type: "stamp",
-        name: "doya-nya"
-      }, {
-        type: "stamp",
-        name: "hyun-nya"
-      },
-    );
     robot.send({ userID: "236fe853-f208-477b-9f1f-0f42fe614d3b" }, ":choo-choo-train-nya:");
   });
   robot.respond(/send$/i, res => {
     robot.send({ channelID: "06a78616-4de5-4195-826d-ad834912e215" }, ":good-nya:");
   });
-  robot.hear(/stamp$/, res =>{
-    res.send(
-      {
-        type: "stamp",
-        name: "done-nya"
-      }
-    );
+
+  robot.hear(/stamp$/, res => {
+    res.send({
+      type: "stamp",
+      name: "done-nya"
+    });
   });
-  robot.hear(/stamps$/, res =>{
-    res.send(
-      {
-        type: "stamp",
-        name: "done-nya"
-      }, {
-        type: "stamp",
-        name: "doya-nya"
-      }, {
-        type: "stamp",
-        name: "hyun-nya"
-      },
-    );
+  robot.hear(/stamps$/, res => {
+    res.send({
+      type: "stamp",
+      name: "done-nya"
+    }, {
+      type: "stamp",
+      name: "doya-nya"
+    }, {
+      type: "stamp",
+      name: "hyun-nya"
+    });
   });
 }
