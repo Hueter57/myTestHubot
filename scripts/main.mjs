b3f9bcc5f21db86d729a5020756bf70440b4fdb6
Hueter <kohsukemori@gmail.com>
2025-02-16 19:24:31 +0900

    🍞 add stamps

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -23,11 +23,33 @@ export default async robot => {
       }, {
         type: "stamp",
         name: "hyun-nya"
-      }
+      },
     );
     robot.send({ userID: "236fe853-f208-477b-9f1f-0f42fe614d3b" }, ":choo-choo-train-nya:");
   });
   robot.respond(/send$/i, res => {
     robot.send({ channelID: "06a78616-4de5-4195-826d-ad834912e215" }, ":good-nya:");
+  });
+  robot.hear(/stamp$/, res =>{
+    res.send(
+      {
+        type: "stamp",
+        name: "done-nya"
+      }
+    );
+  });
+  robot.hear(/stamps$/, res =>{
+    res.send(
+      {
+        type: "stamp",
+        name: "done-nya"
+      }, {
+        type: "stamp",
+        name: "doya-nya"
+      }, {
+        type: "stamp",
+        name: "hyun-nya"
+      },
+    );
   });
 }
