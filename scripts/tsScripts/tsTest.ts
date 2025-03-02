90ff87c2e80889a9963d8fb78d7d9d37cbc6ea60
Hueter <kohsukemori@gmail.com>
2025-03-02 23:21:01 +0900

    🍞 add node-traq and test

--- scripts/tsScripts/tsTest.ts
+++ scripts/tsScripts/tsTest.ts
@@ -5,11 +5,21 @@
 
 import hubot from "hubot";
 
+import { Apis, Configuration } from "@traptitech/traq";
+
+const api = new Apis(new Configuration({
+  accessToken: process.env.HUBOT_TRAQ_ACCESS_TOKEN
+}));
+
+
 module.exports = (robot: hubot.Robot): void => {
   robot.hear(/ping$/i, async (res: hubot.Response): Promise<void> => {
     await res.send("pong");
   }); 
   robot.hear(/hoge$/i, async (res: hubot.Response): Promise<void> => {
     await res.reply("fuga");
+    api.getStamps().then(res => {
+      console.log(res);
+    });
   });
 };
--- scripts/tsTest.js
+++ scripts/tsTest.js
@@ -4,11 +4,18 @@
 // Commands:
 //
 Object.defineProperty(exports, "__esModule", { value: true });
+const traq_1 = require("@traptitech/traq");
+const api = new traq_1.Apis(new traq_1.Configuration({
+    accessToken: process.env.HUBOT_TRAQ_ACCESS_TOKEN
+}));
 module.exports = (robot) => {
     robot.hear(/ping$/i, async (res) => {
         await res.send("pong");
     });
     robot.hear(/hoge$/i, async (res) => {
         await res.reply("fuga");
+        api.getStamps().then(res => {
+            console.log(res);
+        });
     });
 };
