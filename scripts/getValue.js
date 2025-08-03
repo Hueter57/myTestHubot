1b3b16f0ed72dcb626cde75f59c35b174f556545
Hueter <kohsukemori@gmail.com>
2025-08-03 16:13:07 +0900

    🍥 po

--- scripts/getValue.js
+++ scripts/getValue.js
@@ -7,9 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 module.exports = (robot) => {
     robot.hear(/getValue (.+)$/i, async (res) => {
         const value = res.match.join(", ");
-        const num = res.match[2];
-        const input = res.match[3];
-        res.send(`value : ${value}, index: ${num}, input: ${input}`);
+        res.send(`value : ${value}`);
     });
     robot.hear(/getValues ([^,]+),([^,]+)$/i, async (res) => {
         const value = res.match.join(", ").split(", ");
--- scripts/tsScripts/getValue.ts
+++ scripts/tsScripts/getValue.ts
@@ -9,9 +9,7 @@ import hubot from "hubot";
 module.exports = (robot: hubot.Robot): void => {
   robot.hear(/getValue (.+)$/i, async (res: hubot.Response): Promise<void> => {
     const value = res.match.join(", ");
-    const num = res.match[2];
-    const input = res.match[3];
-    res.send(`value : ${value}, index: ${num}, input: ${input}`);
+    res.send(`value : ${value}`);
   });
 
   robot.hear(/getValues ([^,]+),([^,]+)$/i, async (res: hubot.Response): Promise<void> => {
