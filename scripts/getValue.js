8d72b2343d8c2cfc7e2030ce088591765a149d93
Hueter <kohsukemori@gmail.com>
2025-08-03 16:04:32 +0900

    🍞 add getValues test

--- scripts/getValue.js
+++ scripts/getValue.js
@@ -7,6 +7,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 module.exports = (robot) => {
     robot.hear(/getValue (.+)$/i, async (res) => {
         const value = res.match.join(", ");
-        res.send(`The value is: ${value}`);
+        const num = res.match[2].index;
+        const input = res.match[3];
+        res.send(`value : ${value}, index: ${num}, input: ${input}`);
+    });
+    robot.hear(/getValues ([^,]+),([^,]+)$/i, async (res) => {
+        const value = res.match.join(", ");
+        const num = res.match[2].index;
+        const input = res.match[3];
+        res.send(`value : ${value}, index: ${num}, input: ${input}`);
     });
 };
--- scripts/tsScripts/getValue.ts
+++ scripts/tsScripts/getValue.ts
@@ -9,6 +9,15 @@ import hubot from "hubot";
 module.exports = (robot: hubot.Robot): void => {
   robot.hear(/getValue (.+)$/i, async (res: hubot.Response): Promise<void> => {
     const value = res.match.join(", ");
-    res.send(`The value is: ${value}`);
+    const num = res.match[2].index;
+    const input = res.match[3];
+    res.send(`value : ${value}, index: ${num}, input: ${input}`);
+  });
+
+  robot.hear(/getValues ([^,]+),([^,]+)$/i, async (res: hubot.Response): Promise<void> => {
+    const value = res.match.join(", ");
+    const num = res.match[2].index;
+    const input = res.match[3];
+    res.send(`value : ${value}, index: ${num}, input: ${input}`);
   });
 }
